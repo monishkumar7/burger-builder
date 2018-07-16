94f882987244e714b848cb3559fedfbc745fe644
monishkumar <monishkumar1154@gmail.com>
2018-07-16 05:55:26 -0400

    add basic routing

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -91,28 +91,29 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         //alert("You are being continued. Please wait for a long time as this feature is currently under development.");
-        this.setState({loading: true});
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Test Person',
-                address: {
-                    street: 'Dubai cross street',
-                    zipcode: '23252',
-                    country: 'England'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-            })
-            .catch(error => {
-                this.setState({loading: false, purchasing: false});
-            });
+        // this.setState({loading: true});
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Test Person',
+        //         address: {
+        //             street: 'Dubai cross street',
+        //             zipcode: '23252',
+        //             country: 'England'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        //     })
+        //     .catch(error => {
+        //         this.setState({loading: false, purchasing: false});
+        //     });
+        this.props.history.push('/checkout');
     }
 
     render() {
