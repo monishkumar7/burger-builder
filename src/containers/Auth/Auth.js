b4d6e4c2b7c95d7201ebd210b7c45f3103399f80
monishkumar <monishkumar1154@gmail.com>
2018-07-25 13:49:13 -0400

    add basic firebase auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -35,8 +35,9 @@ class Auth extends Component {
                 value: '',
                 valid: false,
                 touched: false
-            },
-        }
+            }
+        },
+        isSignup: true
     }
 
     checkValidity (value, rules) {
@@ -72,7 +73,15 @@ class Auth extends Component {
 
     submitHandler = (event) => {
         event.preventDefault();
-        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value);
+        this.props.onAuth(this.state.controls.email.value, this.state.controls.password.value, this.state.isSignup);
+    }
+
+    switchAuthModeHandler = () => {
+        this.setState(prevState => {
+            return {
+                isSignup: !prevState.isSignup
+            };
+        });
     }
 
     render() {
@@ -101,6 +110,9 @@ class Auth extends Component {
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
+                    <Button
+                        clicked={this.switchAuthModeHandler}
+                        btnType="Danger">SWITCH TO {this.state.isSignup ? 'SIGN IN' : 'SIGN UP'}</Button>
                 </form>
             </div>
         )
@@ -115,7 +127,7 @@ class Auth extends Component {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onAuth: (email, password) => dispatch(actions.auth(email, password))
+        onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     }
 }
 
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -20,8 +21,25 @@ export const authFail  = (error) => {
     }
 };
 
-export const auth  = (email, password) => {
+export const auth  = (email, password, isSignup) => {
     return dispatch =>  {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        let url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDS4CtnjSoM08dU60CdH7zO56NBEAk0i38';
+        if(!isSignup)
+            url = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=AIzaSyDS4CtnjSoM08dU60CdH7zO56NBEAk0i38';
+        axios.post(url, authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authFail(err));
+            })
     };
 };
