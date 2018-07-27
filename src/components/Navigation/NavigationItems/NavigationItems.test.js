6e130ab15d6fa9079fd46fc73eb565ee7d90de6a
monishkumar <monishkumar1154@gmail.com>
2018-07-27 10:47:16 -0400

    add enzyme and first test

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+    it('should render two <NavigationItem /> elements when not logged in', () => {
+        const wrapper = shallow(<NavigationItems />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render three <NavigationItem /> elements when logged in', () => {
+        const wrapper = shallow(<NavigationItems auth />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+});
